d186dedd67f4c1cd8d75b9ea572194dcf3749c3a
Jack Hubert <jphubert93@gmail.com>
2021-10-21 16:06:26 -0600

    updated more rubocop corrections

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class Wrestler < ApplicationRecord
 end
--- db/migrate/20211021213242_create_wrestlers.rb
+++ db/migrate/20211021213242_create_wrestlers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateWrestlers < ActiveRecord::Migration[6.1]
   def change
     create_table :wrestlers do |t|
