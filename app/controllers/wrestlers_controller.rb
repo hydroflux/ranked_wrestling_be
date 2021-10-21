1e50cf617ae02b10b4bea48e57c4149fde89b20b
Jack Hubert <jphubert93@gmail.com>
2021-10-21 16:15:44 -0600

    greated a 'create' method for teh 'wrestlers_controller' along with a private 'wrestler_params' method

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -13,4 +13,28 @@ class WrestlersController < ApplicationController
     render json: @wrestler
   end
 
+  def create
+    @wrestler = Wrestler.new(wrestler_params)
+    if @wrestler.valid?
+      @wrestler.save
+      render json: @wrestler, message: "#{@wrestler.name} created."
+    else
+      render json: { errors: @wrestler.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def wrestler_params
+    params.require(:wrestler).permit(
+      :name,
+      :team,
+      :league,
+      :division,
+      :level,
+      :weight,
+      :win,
+      :loss
+    )
+  end
 end
