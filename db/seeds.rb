79a3005020cac2a661f5a451de96d29e7ba62b9a
Jack Hubert <jphubert93@gmail.com>
2021-10-21 15:59:58 -0600

    created wrestler seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,91 @@
 # frozen_string_literal: true
+
+Wrestler.destroy_all
+
+# Lincoln Heights
+Wrestler.create(
+  name: 'Adam', team: 'Lincoln Heights', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 115, win: 4, loss: 6
+)
+Wrestler.create(
+  name: 'Tom', team: 'Lincoln Heights', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 135, win: 2, loss: 8
+)
+Wrestler.create(
+  name: 'Sebastian', team: 'Lincoln Heights', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 125, win: 5, loss: 5
+)
+Wrestler.create(
+  name: 'Jimmy', team: 'Lincoln Heights', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 165, win: 5, loss: 5
+)
+Wrestler.create(
+  name: 'Tyler', team: 'Lincoln Heights', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 185, win: 7, loss: 3
+)
+
+# Hawthorne Falls
+Wrestler.create(
+  name: 'Alexander', team: 'Hawthorne Falls', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 115, win: 1, loss: 9
+)
+Wrestler.create(
+  name: 'Luke', team: 'Hawthorne Falls', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 125, win: 6, loss: 4
+)
+Wrestler.create(
+  name: 'Noah', team: 'Hawthorne Falls', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 135, win: 7, loss: 3
+)
+Wrestler.create(
+  name: 'Oliver', team: 'Hawthorne Falls', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 165, win: 5, loss: 5
+)
+Wrestler.create(
+  name: 'Lucas', team: 'Hawthorne Falls', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 185, win: 6, loss: 4
+)
+
+# Westwood
+Wrestler.create(
+  name: 'Nolan', team: 'Westwood', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 115, win: 9, loss: 1
+)
+Wrestler.create(
+  name: 'Roman', team: 'Westwood', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 125, win: 7, loss: 3
+)
+Wrestler.create(
+  name: 'Connor', team: 'Westwood', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 135, win: 8, loss: 2
+)
+Wrestler.create(
+  name: 'Robert', team: 'Westwood', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 165, win: 3, loss: 7
+)
+Wrestler.create(
+  name: 'Jordan', team: 'Westwood', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 185, win: 5, loss: 5
+)
+
+# Storm Coast
+Wrestler.create(
+  name: 'Axel', team: 'Storm Coast', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 115, win: 1, loss: 9
+)
+Wrestler.create(
+  name: 'Leonardo', team: 'Storm Coast', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 125, win: 6, loss: 4
+)
+Wrestler.create(
+  name: 'Colton', team: 'Storm Coast', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 135, win: 2, loss: 8
+)
+Wrestler.create(
+  name: 'Jack', team: 'Storm Coast', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 165, win: 10, loss: 0
+)
+Wrestler.create(
+  name: 'Hunter', team: 'Storm Coast', league: '2A', division: 'OSAA',
+  level: 'Varsity', weight: 185, win: 3, loss: 7
+)
