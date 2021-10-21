864d3cc78a464b36f0f6e32cc835ba8e0ff01869
Jack Hubert <jphubert93@gmail.com>
2021-10-21 16:26:56 -0600

    updated the wrestler model

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Wrestler < ApplicationRecord
+  validates :name, presence: true
+  validates :team, presence: true
+  validates :league, presence: true
+  validates :division, presence: true
+  validates :level, presence: true
+  validates :weight, presence: true
+  validates :win, presence: true
+  validates :loss, presence: true
+
+  # has_many :matches
+  # has_many :events, through: :matches
+  # belongs_to: :team
 end
