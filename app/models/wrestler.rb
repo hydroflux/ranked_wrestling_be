8d835991ec75f6fa7b4f31b4de01179aa3d972fe
Jack Hubert <jphubert93@gmail.com>
2021-10-22 09:58:58 -0600

    updated the 'wrestler' model

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -2,9 +2,6 @@
 
 class Wrestler < ApplicationRecord
   validates :name, presence: true
-  validates :team, presence: true
-  validates :league, presence: true
-  validates :division, presence: true
   validates :level, presence: true
   validates :weight, presence: true
   validates :win, presence: true
