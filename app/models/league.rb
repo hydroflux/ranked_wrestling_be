2f9c0be013d7878e4981123fd5d10c2a4feb1a20
Jack Hubert <jphubert93@gmail.com>
2021-10-22 09:32:11 -0600

    started working through the 'league' model

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,8 @@
 class League < ApplicationRecord
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :division, presence: true
+  validates :division_abbreviation, presence: true
+
+  
 end
